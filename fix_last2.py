0b55b60602253c38b0d43e3d0cbf02c9f2eb597b
namnh <ninhhainam12@gmail.com>
2026-04-22 00:16:01 -0700

    chore: Remove deprecated code files related to historical and code fields

--- fix_last2.py
+++
@@ -1,5 +0,0 @@
-﻿import os, re
-f = 'src/main/java/com/hainam/worksphere/diseasehistory/service/DiseaseHistoryService.java'
-with open(f, 'r', encoding='utf-8') as fh: c = fh.read()
-c = re.sub(r'\.historyCode\(null\)', '', c)
-with open(f, 'w', encoding='utf-8') as fh: fh.write(c)
--- fix_str.py
+++
@@ -1,8 +0,0 @@
-﻿import sys
-f = 'src/main/java/com/hainam/worksphere/diseasehistory/service/DiseaseHistoryService.java'
-with open(f, 'r', encoding='utf-8') as fh: c = fh.read()
-c = c.replace('.historyCode(r.getHistoryCode())', '')
-c = c.replace('.diseaseCode(r.getDiseaseCode())', '')
-c = c.replace('if (r.getDiseaseCode() != null) e.setDiseaseCode(r.getDiseaseCode());', '')
-c = c.replace('if (repo.existsActiveByHistoryCode(r.getHistoryCode())) throw new BusinessRuleViolationException("Disease history code already exists: " + r.getHistoryCode());', '')
-with open(f, 'w', encoding='utf-8') as fh: fh.write(c)
